cccf8bfb123c6c680d15a2fa0ec6a1f7dd581a51
Samanala <isuruody@gmail.com>
2022-11-02 15:59:04 +0530

    [INIT] - Student registration

---
+++ _manifest_.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Student Management',
+    'version' : '15.1.0.0',
+    'sequence': 185,
+    'category': 'Coustermization',
+    'website' : 'https://www.odoo.com/app/fleet',
+    'author' : 'Isuru Samarakkody',
+    'summary' : 'Manage Student Profile',
+    'description' : """""",
+    'depends': [
+          'base'
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ models/student_details.py
@@ -0,0 +1,9 @@
+from odoo import api, fields, models, _
+
+
+class StudentDetails(models.Model):
+    _name = "student.details"
+    _description = "Manage Student Details"
+
+    name = fields.Char(string='Full Name', required=True)
+    age = fields.Integer(string='Age', required=True)
